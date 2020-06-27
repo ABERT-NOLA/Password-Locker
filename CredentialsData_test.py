86a3000b6970571f43a78fae34bf2ff02b7a55f3
Albert Obwoge <albertnyambegaa@gmail.com>
2020-06-27 16:06:22 +0300

    added tearDown

--- CredentialsData_test.py
+++ CredentialsData_test.py
@@ -15,3 +15,15 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credential.platform, "Gmail")
         self.assertEqual(self.new_credential.username, "cliff")
         self.assertEqual(self.new_credential.password, "lolololo")    
+
+    def tearDown(self):
+    
+        CredentialsData.credentials = []
+
+    def test_save_credential(self):
+        """
+        test if credential is saved in the credentials list
+        """
+        self.new_credential.save_credential()  
+        self.assertEqual(len(CredentialsData.credentials), 1)
+    
