515a61789513ed33cd433f7ede0f73540860f3e9
Albert Obwoge <albertnyambegaa@gmail.com>
2020-06-27 16:05:02 +0300

    updates to the class

--- CredentialsData_test.py
+++ CredentialsData_test.py
@@ -1,2 +1,17 @@
 import unittest
 from CredentialsData import CredentialsData
+
+class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        """
+        setUp method
+        """
+        self.new_credential = CredentialsData("Gmail", "cliff", "lolololo")
+        
+    def test_init(self):
+        """
+        testing initialization
+        """
+        self.assertEqual(self.new_credential.platform, "Gmail")
+        self.assertEqual(self.new_credential.username, "cliff")
+        self.assertEqual(self.new_credential.password, "lolololo")    
