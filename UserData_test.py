16e80d8c798781cf4b92e97f3b2d554faf240df3
Albert Obwoge <albertnyambegaa@gmail.com>
2020-06-27 14:44:17 +0300

    added test _init

--- UserData_test.py
+++ UserData_test.py
@@ -1,5 +1,5 @@
-import unittest # Importing the unittest module
-from UserData import UserData # Importing the contact class
+import unittest
+from UserData import UserData
 
 class TestuserData(unittest.TestCase):
     def setUp(self):
@@ -7,5 +7,15 @@ class TestuserData(unittest.TestCase):
         set up method
         """
         self.new_user = UserData("cliff", "kasera", "ckasera@gmail.com", "Blankphrase", "lololo") 
+
+    def test_init(self):
+        """
+        testing initialization of class
+        """
+        self.assertEqual(self.new_user.first_name, "cliff")
+        self.assertEqual(self.new_user.last_name, "kasera")
+        self.assertEqual(self.new_user.email, "ckasera@gmail.com")
+        self.assertEqual(self.new_user.user_name, "Blankphrase")
+        self.assertEqual(self.new_user.pass_word, "lololo")
 
     
