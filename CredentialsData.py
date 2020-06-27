3c2f66eb9e1c64c31226d8bb7d6ddce3e06cd695
Albert Obwoge <albertnyambegaa@gmail.com>
2020-06-27 15:46:52 +0300

    class updates

---
+++ CredentialsData.py
@@ -0,0 +1,20 @@
+class CredentialsData:
+
+    """
+    create new instances
+    """
+
+    credentials = []
+
+    def __init__(self, platform, username, password):
+
+        self.platform = platform
+        self.username = username
+        self.password = password
+
+    def save_credential(self):
+
+        """
+        save credential objects to the credential list
+        """
+        CredentialsData.credentials.append(self) 
