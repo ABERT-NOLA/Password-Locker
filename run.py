cb5c98fdd1ed5d737d00a1ada7ebd306bd8e659c
Albert Obwoge <albertnyambegaa@gmail.com>
2020-06-27 16:29:11 +0300

    added save accounts

--- run.py
+++ run.py
@@ -14,3 +14,8 @@ def new_users(name_one, name_two, email_address, user_name, pass_word):
     new_user = UserData(name_one, name_two, email_address, user_name, pass_word)
     
     return new_user
+def save_accounts(account):
+    """
+    save new user account
+    """
+    account.save_account()
