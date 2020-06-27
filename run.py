c1888b25f4d1c203486fac80260e8aa09dce5194
Albert Obwoge <albertnyambegaa@gmail.com>
2020-06-27 16:13:53 +0300

    added new_users

--- run.py
+++ run.py
@@ -5,3 +5,12 @@ import string
 
 from UserData import UserData
 from CredentialsData import CredentialsData
+
+def new_users(name_one, name_two, email_address, user_name, pass_word):
+
+    """
+    creates new user 
+    """
+    new_user = UserData(name_one, name_two, email_address, user_name, pass_word)
+    
+    return new_user
